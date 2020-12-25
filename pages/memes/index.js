4ed4b69c75f3245b9b8e950c8d2076398bf2953b
balatzar <bataprod@gmail.com>
2020-12-25 09:39:43 +0100

    Basic masonry

--- pages/memes/index.js
+++ pages/memes/index.js
@@ -1,6 +1,7 @@
 import { fetchMemes } from "../../services/airtable";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export async function getStaticProps() {
   const allMemes = await fetchMemes();
@@ -14,18 +15,24 @@ export async function getStaticProps() {
 export default function Index({ allMemes }) {
   return (
     <div>
+      <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
       <div>
         <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
           Le cru 2020
         </h1>
-        {allMemes.map((meme) => {
-          const { width, height, url } = meme.image[0].thumbnails.large;
-          return (
-            <div key={meme.id}>
-              <Image src={url} width={width} height={height} />
-            </div>
-          );
-        })}
+        <div
+          className="grid mx-auto my-0"
+          data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 10 }'
+        >
+          {allMemes.map((meme) => {
+            const { width, height, url } = meme.image[0].thumbnails.large;
+            return (
+              <div key={meme.id} className="grid-item">
+                <Image src={url} width={width} height={height} />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
