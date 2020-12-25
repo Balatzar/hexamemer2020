3012c7075c376855d549656415cd1678263b032c
balatzar <bataprod@gmail.com>
2020-12-25 10:02:35 +0100

    Meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,33 @@
 import "../styles/index.css";
 import DarkModeToggle from "../components/darkModeToggle";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <title>Hexamemer 2020</title>
+        <meta
+          name="description"
+          content="Votez pour élire le ou la meilleur/e Memer de 2020"
+        />
+        <meta property="og:title" content="Hexamemer 2020" key="ogtitle" />
+        <meta
+          property="og:description"
+          content="Votez pour élire le ou la meilleur/e Memer de 2020"
+          key="ogdesc"
+        />
+        <meta
+          property="og:url"
+          content="https://hexamemer2020.vercel.app"
+          key="ogurl"
+        />
+        <meta
+          property="og:site_name"
+          content="Hexamemer 2020"
+          key="ogsitename"
+        />
+      </Head>
       <DarkModeToggle />
       <Component {...pageProps} />
     </div>
